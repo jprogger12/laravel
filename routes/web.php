4f3260c06dbe64b6f195abeed2a1f127d6f63153
jpogger12 <usmonali1971@mail.ru>
2020-09-24 00:26:10 +0500

    test for nitro 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\mainController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return "<h2> salom salom </h2>" ;
-});
+Route::get('api', [mainController::class, 'head']);
